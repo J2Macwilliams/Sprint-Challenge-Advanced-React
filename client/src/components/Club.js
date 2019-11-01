14c0dc2f84a007275ab496ae48d92101bd5f8a6d
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-01 09:09:29 -0700

    Create Club component. Import Players component. Create lifecycle componentDidMount to make axios call. Change axios call number to 6000.

---
+++ client/src/components/Club.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+import Players from './Players'
+
+
+class Club extends Component {
+    state = {
+        players: [],
+        name: ''
+
+    }
+
+    componentDidMount() {
+
+        axios
+            .get('http://localhost:5000/api/players')
+            .then(response => {
+                // console.log(response)
+                this.setState({
+                    players: response.data
+                })
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <div>
+                <Players data={this.state.players} />
+            </div>
+        )
+    }
+}
+export default Club
