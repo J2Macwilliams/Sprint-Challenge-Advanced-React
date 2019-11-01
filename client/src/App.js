697a9449b01bece110278da8c82ba879ad2d6392
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-01 08:44:55 -0700

    Add Club route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Club from './components/Club';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Route path='' render={(props) => <Club {...props}/>}/>
     </div>
   );
 }
