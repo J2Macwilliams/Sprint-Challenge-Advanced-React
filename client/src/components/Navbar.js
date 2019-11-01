5cfd4873cb19908fa6e6106f5c3dc6edf95dffc2
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-01 08:51:52 -0700

    Create NavBar Component. Add NavLink for Home and Players. Import useDarkMode hook. set up toggle function. Call toggle function.

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import useDarkMode  from '../hooks/useDarkMode';
+
+const Navbar = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  };
+  return (
+    <nav className="navbar">
+      <h1>Women's World Cup</h1>
+      <NavLink exact to='/' activeClassName="activeNavButton" className="navLink" >
+        Home
+      </NavLink>
+      <NavLink to='/charts-page' activeClassName="activeNavButton" className="navLink" >Players</NavLink>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
