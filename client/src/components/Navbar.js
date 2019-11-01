b3ee9f183704af2dbe55222f63609dced452160d
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-01 10:58:35 -0700

    Run test, got only 1 back??

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = () => {
   };
   return (
     <nav className="navbar">
+      
       <h1 className="title">Women's World Cup</h1>
       <NavLink exact to='/' activeClassName="activeNavButton" className="navLink" >
         Home
